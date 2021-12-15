a39b4aaf8cf4ed040291f1dfb3cfdf1ad20ad610
Jason Hargrove <rijaha@gmail.com>
2021-12-15 14:34:35 -0600

    Export the component challenge.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,13 +7,17 @@ function Profile() {
     );
 }
 
+function Profile2() {
+    return <img src="https://i.imgur.com/lICfvbD.jpg" alt="Aklilu Lemma" />
+}
+
 export default function Gallery() {
     return (
         <section>
             <h1>Amazing Scientists</h1>
             <Profile />
             <Profile />
-            <Profile />
+            <Profile2 />
         </section>
     );
 }
