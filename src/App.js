c269ad97c2546318fc9de82746df6da206855795
Jason Hargrove <rijaha@gmail.com>
2021-12-17 19:02:52 -0600

    Convert some HTML to JSX.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Profile from "./components/Profile";
 import Gallery from "./components/Gallery";
 import { Jason } from "./components/Gallery";
+import ToDoList from "./components/ToDoList";
+import Bio from "./components/Bio";
 
 function App() {
   return (
@@ -9,11 +11,13 @@ function App() {
       <Profile />
       <Gallery />
       <Jason />
+      <ToDoList />
+      <Bio />
     </div>
   );
 }
 
 // Left off at Writing Markup with JSX
-// https://beta.reactjs.org/learn/importing-and-exporting-components
+// https://beta.reactjs.org/learn/writing-markup-with-jsx
 
 export default App;
