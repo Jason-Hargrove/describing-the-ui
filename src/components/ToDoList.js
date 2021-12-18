ee5af10bd90f5ba1b66e11caa140da6f8822d15e
Jason Hargrove <rijaha@gmail.com>
2021-12-18 09:17:03 -0600

    Completed React challenges.

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -16,12 +16,29 @@ const person = {
     }
 }
 
+const baseUrl = 'https://i.imgur.com/';
+const person2 = {
+    name: 'Gregorio Y. Zara',
+    imageId: '7vQD0fP',
+    imageSize: 's',
+    theme: {
+        margin: '1em',
+        border: '5px solid red',
+        width: '5em',
+        borderRadius: '10% 30% 50% 70%'
+    }
+};
+
 export default function ToDolist() {
     const name = "Hedy Lamarr"
 
     return (
         <>
             <div style={person.theme}>
+                <img style={person2.theme}
+                    src={ baseUrl + person2.imageId + person2.imageSize + ".jpg" }
+                    alt={ person2.name }
+                />
                 <h1>{person.name}'s Theme</h1>
                 <h1>To Do List for {formatDate(today)}</h1>
                 <h1>{name}'s Todos</h1>
