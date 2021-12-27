165fe60cd82d6bb1af694cb87e24ab19b91ebc40
Jason Hargrove <rijaha@gmail.com>
2021-12-27 15:00:04 -0600

    Structured people array.

--- src/components/List.js
+++ src/components/List.js
@@ -1,19 +1,35 @@
-const people = [
-    "Creola Katherine Johnson: mathematician",
-    "Mario José Molina-Pasquel Henríquez: chemist",
-    "Mohammad Abdus Salam: physicist",
-    "Percy Lavon Julian: chemist",
-    "Subrahmanyan Chandrasekhar: astrophysicist"
-];
+const people = [{
+        id: 0,
+        name: "Creola Katherine Johnson",
+        profession: "mathematician"
+    }, {
+        id: 1,
+        name: "Mario José Molina-Pasquel Henríquez",
+        profession: "chemist"
+    }, {
+        id: 2,
+        name: "Mohammad Abdus Salam",
+        profession: "physicist"
+    }, {
+        name: "Percy Lavon Julian",
+        profession: "chemist"
+    }, {
+        name: "Subrahmanyan Chandrasekhar",
+        profession: "astrophysicist"
+    }];
 
-const listItems = people.map(person => <li>{person}</li>);
 
-console.log('groovy')
+
 
 export default function List() {
+    const listItems = people.map(person =>
+        <li>{person}</li>
+    );
     return (
         <>
-            <h1>Hello</h1>
+            <ul className="people-list">
+                {listItems}
+            </ul>
         </>
     );
 }
