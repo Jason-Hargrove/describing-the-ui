7711d67da00ee7a4cc71d7adf69803fb4dea85e1
Jason Hargrove <rijaha@gmail.com>
2021-12-30 22:54:16 -0600

    Extracted repetitive parts into a component.

--- src/components/SplitListFunc.js
+++ src/components/SplitListFunc.js
@@ -2,53 +2,51 @@ import { people3 } from "../data/data3";
 import { getImageUrl } from "./utils3";
 
 // ========== Extract Repetitive Parts Into a <ListSection> Component ==========
+
+function ListSection({ title, people }) {
+    return (
+        <>
+            <h2>{title}</h2>
+            <ul>
+                {people.map(person =>
+                    <li key={person.id}>
+                        <img
+                            src={getImageUrl(person)}
+                            alt={person.name}
+                        />
+                        <p>
+                            <b>{person.name}:</b>
+                            {' ' + person.profession + ' '}
+                            known for {person.accomplishment}
+                        </p>
+                    </li>
+                )}
+            </ul>
+        </>
+    );
+}
+
 export default function SplitListFunc() {
 
 // ===== Chemists =====
     const chemists = people3.filter(person =>
         person.profession === "chemist"
     );
-
-    const listChemists = chemists.map(person =>
-        <li key={person.id}>
-            <img
-                src={getImageUrl(person)}
-                alt={person.name}
-            />
-            <p>
-                <b>{person.name}:</b>
-                {' ' + person.profession + ' '}
-                known for {person.accomplishment}
-            </p>
-        </li>
-    );
-
 // ===== Everyone Else =====
     const everyoneElse =  people3.filter(person =>
         person.profession !== "chemist"
     );
-
-    const listEveryoneElse = everyoneElse.map(person =>
-        <li key={person.id}>
-            <img
-                src={getImageUrl(person)}
-                alt={person.name}
-            />
-            <p>
-                <b>{person.name}:</b>
-                {' ' + person.profession + ' '}
-                known for {person.accomplishment}
-            </p>
-        </li>
-    );
-
     return (
         <article className="split-list">
             <h1>Scientists - <span style={{color: "blue"}}>SplitListFunc</span></h1>
-            <h2>Chemists</h2>
-            <ul>{listChemists}</ul>
-            <h2>Everyone Else</h2>
-            <ul>{listEveryoneElse}</ul>
+            <ListSection
+                title="Chemists"
+                people={chemists}
+            />
+            <ListSection
+                title="Everyone Else"
+                people={everyoneElse}
+            />
         </article>
     );
 }
