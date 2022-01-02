9333da8d1c9452d34be59560ef40d09462eaab55
Jason Hargrove <rijaha@gmail.com>
2022-01-02 11:24:39 -0600

    Completed nested lists in one component.

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,17 +1,26 @@
+// Challenge 2 of 4: Nested lists in one component
+// Make a list of recipes from this array! For each recipe in the array, display its title as an <h2> and list its ingredients in a <ul>.
+
 import { recipes } from "../data/recipeData.js";
+import {Fragment} from "react";
 
 export default function RecipeList() {
 
     const ListOfRecipes = recipes.map(recipe =>
-        <li>
-            <b>{recipe.name}</b>
-        </li>
+       <>
+           <h2>{recipe.name}</h2>
+           <ul>
+             <li>
+               {recipe.ingredients.join(", ")}
+             </li>
+            </ul>
+       </>
     );
 
     return (
-        <div>
+        <div className="recipe-list">
             <h1>Recipes</h1>
-            <ul>{ListOfRecipes}</ul>
+            {ListOfRecipes}
         </div>
     );
 }
